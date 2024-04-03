7a04057f3115b0b2625b45ba41ac869f7ead3c9b
jmazoyer <jmz.mazoyer@gmail.com>
2024-04-03 18:17:18 +0200

    Feat(Game): Add a clickable link on the winner

--- front-end/src/components/game/GameResult.tsx
+++ front-end/src/components/game/GameResult.tsx
@@ -1,5 +1,6 @@
 import dashStyles from '@/styles/dash.module.css';
 import gameStyles from '@/styles/game.module.css';
+import Link from 'next/link';
 
 interface GameResultProps {
   winner: {
@@ -12,23 +13,25 @@ interface GameResultProps {
 export const GameResult = ({ winner }: GameResultProps): JSX.Element => {
   return (
     <div className={gameStyles.ctn__result}>
-      <div className={gameStyles.ctn__result_img}>
-        <picture>
-          <img
-            className={dashStyles.img__prof}
-            src={winner.profilePicture}
-            alt="Winner picture"
-          />
-        </picture>
-      </div>
-      <div className={gameStyles.ctn__result_txt}>
-        <div className={gameStyles.ctn__result_txt_title}>
-          <h2 className={gameStyles.result_h2}>{winner.username}</h2>
+      <Link href={`/profile/${winner.id}`}>
+        <div className={gameStyles.ctn__result_img}>
+          <picture>
+            <img
+              className={dashStyles.img__prof}
+              src={winner.profilePicture}
+              alt="Winner picture"
+            />
+          </picture>
         </div>
-        <div className={gameStyles.ctn__result_txt_text}>
-          <p> has won the game!</p>
+        <div className={gameStyles.ctn__result_txt}>
+          <div className={gameStyles.ctn__result_txt_title}>
+            <h2 className={gameStyles.result_h2}>{winner.username}</h2>
+          </div>
+          <div className={gameStyles.ctn__result_txt_text}>
+            <p> has won the game!</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
