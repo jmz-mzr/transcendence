109394e00ea8eaa5dd2077725503c65075dc1b69
jmazoyer <jmz.mazoyer@gmail.com>
2024-04-04 10:45:15 +0200

    Feat(Front): Update Next.js
    And also:
    - Enable 'standalone' output mode, to create a smaller production docker image

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -7,8 +7,9 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    // ignoreDuringBuilds: true,
   },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
