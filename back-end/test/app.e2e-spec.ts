9f2d8f7738b17e86d43b73a957d7d9bf71814d3c
jmazoyer <jmz.mazoyer@gmail.com>
2024-04-23 16:46:56 +0200

    🔒️ Avoid the vulnerable "formidable@2.1.2" dependency
    - Run the tests with "axios" instead of "supertest" (which depends
    on "superagent", depending on "formidable")

--- back-end/test/app.e2e-spec.ts
+++ back-end/test/app.e2e-spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import axios from 'axios';
+import { Server } from 'http';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let server: Server;
+  let baseURL: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,12 +16,34 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    server = app.getHttpServer();
+    await new Promise<void>((resolve) => server.listen(0, resolve));
+
+    const address = server.address();
+    if (address === null || typeof address === 'string') {
+      throw new Error(
+        'Server is not listening or returned an unexpected address type.',
+      );
+    }
+
+    const port = address.port;
+    baseURL = `http://localhost:${port}`;
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/profile (GET)', async () => {
+    try {
+      const response = await axios.get(`${baseURL}/profile`);
+      expect(response.status).toBe(401);
+    } catch (error) {
+      if (error.response) {
+        expect(error.response.status).toBe(401);
+      } else {
+        throw error;
+      }
+    }
   });
 });
